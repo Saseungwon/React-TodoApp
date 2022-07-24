8e64aa81a96204eb22dbc827558e7bb6c10df54e
dfg1425@naver.com <dfg1425@naver.com>
2022-07-25 00:59:55 +0900

    onToggle

--- src/App.js
+++ src/App.js
@@ -7,17 +7,17 @@ const App = () => {
   const [todos, setTodos] = useState([
     {
       id: 1,
-      text: '리액트의 기초 알아보기',
+      text: 'test1',
       checked: true,
     },
     {
       id: 2,
-      text: '컴포넌트 스타일링해 보기',
+      text: 'test2',
       checked: true,
     },
     {
       id: 3,
-      text: '일정 관리 앱 만들어 보기',
+      text: 'test3',
       checked: false,
     },
   ]);
@@ -37,10 +37,28 @@ const App = () => {
     [todos],
   );
 
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos],
+  );
+
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
+
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
-      <TodoList todos={todos} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
 };
